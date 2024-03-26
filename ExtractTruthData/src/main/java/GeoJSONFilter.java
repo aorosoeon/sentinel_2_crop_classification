f6fe62ad991804f3cdcaafac9cdaa2f5664e3bda
andrewgbeck <andrew.g.beck@gmail.com>
2024-03-26 10:43:25 -0400

    Added optional parameter to specify included crop types
    Added option Include List parameter, hard-coded Exclude List, and more comments

--- ExtractTruthData/src/main/java/GeoJSONFilter.java
+++ ExtractTruthData/src/main/java/GeoJSONFilter.java
@@ -7,26 +7,46 @@ import com.opencsv.CSVWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class GeoJSONFilter {
     public static void main(String[] args) {
-        if (args.length != 3) {
-            System.err.println("Usage: java GeoJSONFilter <GeoJSONFilePath> <BoundingBoxJSON> <OutputCSVFilePath>");
+        if (args.length < 3) {
+            System.err.println("Usage: java GeoJSONFilter <GeoJSONFilePath> <BoundingBoxJSON> <OutputCSVFilePath> <Optional: CSV list of include descriptions (exact match required!)>");
             System.exit(1);
         }
 
         String geoJsonFilePath = args[0];
         String boundingBoxJson = args[1];
         String outputCsvFilePath = args[2];
+        List<String> includeList = new ArrayList<>();
+
+        // If provided, convert the 4th arg to a List of CropTypes to include, and trim spaces
+        // Wrap each item in double quotes to match geoJSON contents
+        if (args.length == 4) {
+            includeList = Stream.of(args[3].split(","))
+                    .map(item -> "\"" + item.trim() + "\"")
+                    .toList();
+        }
+
+        // Hard-coded list of crop types to exclude, if include list is NOT provided
+        // Note double quotes to match contents of geoJSON file
+        List<String> excludeList = List.of("\" \"", "\"Abandoned Agricultural Land\"");
 
         try {
             ObjectMapper objectMapper = new ObjectMapper();
             JsonNode boundingBoxNode = objectMapper.readTree(boundingBoxJson);
 
+            // This is the bounding box for the truth/fact data
+
             double xmin = boundingBoxNode.get("xmin").asDouble();
             double ymin = boundingBoxNode.get("ymin").asDouble();
             double xmax = boundingBoxNode.get("xmax").asDouble();
             double ymax = boundingBoxNode.get("ymax").asDouble();
+
+            // Read the goeJSON truth data file, and extract relevant entries
 
             try (FileReader fileReader = new FileReader(geoJsonFilePath);
                  FileWriter fileWriter = new FileWriter(outputCsvFilePath);
@@ -38,12 +58,18 @@ public class GeoJSONFilter {
                          )
             ) {
 
+                // Write a header row
                 csvWriter.writeNext(new String[]{"Latitude", "Longitude", "CropType"});
 
                 JsonNode geoJsonNode = objectMapper.readTree(fileReader);
                 JsonNode featureNodes = geoJsonNode.get("features");
                 int totalFeatures = featureNodes.size();
                 int processedCount = 0;
+
+                // Extract the coordinates and crop type description
+                // Note that there is an integer code available in attribute CROP_TYP_1,
+                // but we have chosen to use the description for convenience
+                // If processing time/memory is big concern, we could use CROP_TYP_1 instead
 
                 for (JsonNode featureNode : featureNodes) {
                     JsonNode geometryNode = featureNode.get("geometry");
@@ -52,16 +78,26 @@ public class GeoJSONFilter {
                     double longitude = coordinatesNode.get(0).asDouble();
                     double latitude = coordinatesNode.get(1).asDouble();
 
+                    // Ignore any truth data points outside the bounding box
+
                     if (longitude >= xmin && longitude <= xmax && latitude >= ymin && latitude <= ymax) {
+
                         JsonNode propertiesNode = featureNode.get("properties");
                         String description = String.valueOf(propertiesNode.get("DESCRIPT_1"));
 
-                        csvWriter.writeNext(
-                                new String[]{
-                                        String.valueOf(latitude),
-                                        String.valueOf(longitude),
-                                        description
-                                });
+                        // If Include list is provided, check if this description is in the list
+                        // Otherwise, check if this description is in the Exclude list
+
+                        if ((!includeList.isEmpty() && includeList.contains(description)) ||
+                                (includeList.isEmpty() && !excludeList.contains(description))) {
+                            //Found a useful entry, write it to CSV
+                            csvWriter.writeNext(
+                                    new String[]{
+                                            String.valueOf(latitude),
+                                            String.valueOf(longitude),
+                                            description
+                                    });
+                        }
                     }
 
                     processedCount++;
