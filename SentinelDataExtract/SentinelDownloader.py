02e71295b9e0e9823c5bce7f6bd666065f8e5fdf
Mina <minazebarjadan@gmail.com>
2024-03-31 21:30:43 -0400

    SentinelDownloader.py

--- SentinelDataExtract/SentinelDownloader.py
+++ SentinelDataExtract/SentinelDownloader.py
@@ -4,35 +4,37 @@ import numpy as np
 
 # Configure Sentinel Hub with your API key
 config = SHConfig()
-#config.sh_client_id = 'sh-0ff3049a-dffe-4c03-b3bc-aa3818ca95c3'
-#config.sh_client_secret = 'LiPySb7k8WO5ZQVM1HWb80NiI99yo9As'
-config.sh_client_id = '8bede36a-dd3d-46f0-87af-44d67956c880'
-config.sh_client_secret = 'PKwHDIOtPbTXgBD6dvsF6Z2wf1sZntvm'
+config.sh_client_id = '6b13237e-d289-4827-b8d9-5a90aaedb247'
+config.sh_client_secret = 'CfLdUhR08nJGv7kxeA1DvieL0bdsc2Up'
 
 config.save()
+
+config.mask_no_data = True 
 
 if not config.sh_client_id or not config.sh_client_secret:
     print("Warning! To use Process API, please provide the credentials (OAuth client ID and client secret).")
 
-# longitude_min = -64.484032
-# latitude_min = 45.862321
-# longitude_max = -61.776823
-# latitude_max = 47.061006
 
+# #longitude_min = -64.693039
+# longitude_min = -62.693039
+# latitude_min = 45.894866
+# #longitude_max = -61.969033
+# latitude_max = 46.999324
+# latitude_max = 46.5
+    
+    # SW Ontario
+#Bounding box for SW Ontario: http://bboxfinder.com/#42.803462,-81.589966,44.257003,-80.134277
+longitude_min = -81.589966
+latitude_min = 42.803462
+longitude_max = -80.134277
+latitude_max = 44.257003
 
-#longitude_min = -64.693039
-longitude_min = -61.9691
-latitude_min = 45.890
-longitude_max = -61.969033
-#latitude_max = 46.999324
-latitude_max = 45.895
+# Define the bounding box and time range for the satellite imagery - match it to the time range of the truth data
 
-# Define the bounding box and time range for the satellite imagery
-myBBox = geometry.BBox((longitude_min, latitude_min, longitude_max, latitude_max), constants.CRS.WGS84)
 time_interval = ('2023-08-01', '2023-08-31')
 
 # Define the spectral bands to retrieve (e.g., red, green, blue, near-infrared)
-bands = ['B04', 'B03', 'B02', 'B08']  # Sentinel-2 bands for RGB and NIR
+bands = ['B01','B02','B03','B04','B05','B06','B07','B08','B8A','B09','B10','B11','B12']  # Sentinel-2 bands for RGB and NIR
 
 # define evalscript
 myEvalscript = """
@@ -44,24 +46,25 @@ let viz = new HighlightCompressVisualizer(minVal, maxVal);
 
 function setup() {
 return {
-    input: ["B02", "B08", "B11", "dataMask"],
-    output: { bands: 4 }
+    input: ["B01","B02","B03", "B04","B05", "B06", "B07", "B08","B8A","B09","B10","B11","B12"],
+    output: { bands: 13 }
 };
 }
 
 function evaluatePixel(samples) {
-let val = [samples.B11, samples.B08, samples.B02, samples.dataMask];
+let val = [samples.B01,samples.B02,samples.B03,samples.B04,samples.B05, samples.B06,samples.B07,samples.B08,samples.B8A,samples.B09,samples.B10,samples.B11, samples.B12];
 return viz.processList(val);
 }
 """
 
 # data_source='S2L1C',
 
+
+
 # Create a Sentinel Hub request for satellite imagery
 request = SentinelHubRequest(
-    data_folder='C:/Users/andre/IdeaProjects/SentinelDataExtract/data',  # Folder to save downloaded data
+    data_folder='C:/Users',  # Folder to save downloaded data
     evalscript=myEvalscript,
-    # resolution=[10,10],
     input_data=[
         SentinelHubRequest.input_data (
             time_interval=time_interval,
@@ -74,7 +77,7 @@ request = SentinelHubRequest(
         SentinelHubRequest.output_response('default', MimeType.TIFF)
     ],
     bbox=myBBox,
-    #size=(512, 512),  # Image size
+    size=(512, 512),  # Image size
     config=config
 
 )
@@ -83,6 +86,7 @@ sampleData = request.get_data()
 
 # Process the data to extract coordinates and RGB values
 data = np.array(sampleData[0])  # Assuming the response contains the RGB image data
+
 
 # Extract coordinates (lon, lat) for each pixel
 lon_values = np.linspace(myBBox.min_x, myBBox.max_x, data.shape[1])
@@ -93,10 +97,20 @@ lons, lats = np.meshgrid(lon_values, lat_values)
 df = pd.DataFrame({
     'Longitude': lons.flatten(),
     'Latitude': lats.flatten(),
-    'R': data[:, :, 0].flatten(),  # Red channel
-    'G': data[:, :, 1].flatten(),  # Green channel
-    'B': data[:, :, 2].flatten()   # Blue channel
+    'B01': data[:, :, 0].flatten(),
+    'B02': data[:, :, 1].flatten(),  
+    'B03': data[:, :, 2].flatten(),
+    'B04': data[:, :, 3].flatten(),  
+    'B05': data[:, :, 4].flatten(), 
+    'B06': data[:, :, 5].flatten(),   
+    'B07': data[:, :, 6].flatten(),
+    'B08': data[:, :, 7].flatten(),
+    'B8A': data[:, :, 8].flatten(),   
+    'B09': data[:, :, 9].flatten(),
+    'B10': data[:, :, 10].flatten(),    
+    'B11': data[:, :, 11].flatten(),
+    'B12': data[:, :, 12].flatten()      
 })
 
 # Save the DataFrame to a CSV file
-df.to_csv('C:/Users/andre/IdeaProjects/SentinelDataExtract/data/sentinel2_rgb_values.csv', index=False)
+df.to_csv('./sentinel2_rgb_values.csv', index=False)
